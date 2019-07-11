0668c094b599c49532be2f5be2b5605b700f8e74
Oskar <oskar@Oskars-MacBook-Air.local>
2019-07-11 19:44:51 +0800

    Added init logic for retrieving token

--- index.ts
+++ index.ts
@@ -1,24 +1,37 @@
 import axios from 'axios';
 const authenticate = require('./src/AuthenticationHandler');
+const credentialsHandler = new (require('./src/CredentialHandler'));
 const accountOverViewURL = 'https://www.avanza.se/_mobile/account/overview'
 
-authenticate(require('./credentials').identificationNumber)
-    .then((csid: string) => {
-        if ('' === csid) return
-        console.log('Logged in!');
-    })
-    .catch((err: any) => console.error(err));
+credentialsHandler.init()
+    .then(() => credentialsHandler.getToken()
+        .then((token: string) => {
+            console.log('Already logged in!');
+            main(token);
+        })
+        .catch(() => 
+            authenticate(require('./credentials/credentials').identificationNumber)
+                .then((token: string) => {
+                    if ('' === token) return
+                    console.log('Logged in!');
+                    credentialsHandler.setToken(Buffer.from(token, 'utf-8'))
+                    main(token)
+                })
+                .catch((err: any) => console.error(err))))
 
 function getAccountOverview(csid: string): void{
-    axios.get(accountOverViewURL, {
-        headers: {
+        axios.get(accountOverViewURL, {
+            headers: {
             Cookie: 'csid='+csid+';'
         }
     })
-        .then((res: any) => {
-            console.log(res.data);
-        })
-        .catch((err: any) => {
-            console.error(err)
-        });
+    .then((res: any) => {
+        console.log(res.data);
+    })
+    .catch((err: any) => {
+        console.error(err)
+    });
+}
+
+function main(token: string){
 }
